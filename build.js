8f0f9baeb71638b8d6db7f2eb5fc1ef0e68d36b5
Zoee Silcock <mrzoee@gmail.com>
2021-12-29 09:17:45 +0100

    Add a 404 page

--- build.js
+++ build.js
@@ -2,14 +2,20 @@ import { readFileSync, writeFileSync, copyFileSync } from 'fs';
 import { markdown } from 'markdown';
 import Handlebars from 'handlebars';
 
-// Load the page contents.
-const pageContent = readFileSync('src/index.md', 'utf8');
-
-// Load the html template.
-const template = Handlebars.compile(readFileSync('src/index.hbs', 'utf8'));
-
-// Generate the html.
-writeFileSync('dist/index.html', template({ content: markdown.toHTML(pageContent) }));
+// Build our HTML pages.
+buildHtmlFromMarkdown('index');
+buildHtmlFromMarkdown('404');
 
 // Copy the CSS.
 copyFileSync('src/styles.css', 'dist/styles.css');
+
+function buildHtmlFromMarkdown(fileName) {
+  // Load the page contents.
+  const pageContent = readFileSync(`src/${fileName}.md`, 'utf8');
+
+  // Load the html template.
+  const template = Handlebars.compile(readFileSync('src/template.hbs', 'utf8'));
+
+  // Generate the html.
+  writeFileSync(`dist/${fileName}.html`, template({ content: markdown.toHTML(pageContent) }));
+}
