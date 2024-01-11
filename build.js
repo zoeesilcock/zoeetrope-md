36e17d9139b402418995dbef2b04e7c68c50540a
Zoee Silcock <mrzoee@gmail.com>
2024-01-11 18:58:25 +0100

    Replace markdown-js with more a maintained package

--- build.js
+++ build.js
@@ -1,6 +1,11 @@
 import { readFileSync, writeFileSync, copyFileSync } from 'fs';
-import { markdown } from 'markdown';
+import markdownit from 'markdown-it';
 import Handlebars from 'handlebars';
+
+const md = markdownit({
+  html: true,
+  typographer: true
+});
 
 // Build our HTML pages.
 buildHtmlFromMarkdown('index');
@@ -17,5 +22,5 @@ function buildHtmlFromMarkdown(fileName) {
   const template = Handlebars.compile(readFileSync('src/template.hbs', 'utf8'));
 
   // Generate the html.
-  writeFileSync(`dist/${fileName}.html`, template({ content: markdown.toHTML(pageContent) }));
+  writeFileSync(`dist/${fileName}.html`, template({ content: md.render(pageContent) }));
 }
