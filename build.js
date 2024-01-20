a2838db895992eafb36bd87f692435536aa4ffdc
Zoee Silcock <mrzoee@gmail.com>
2024-01-20 10:30:41 +0100

    Inline the CSS to improve loading speed further

--- build.js
+++ build.js
@@ -7,23 +7,28 @@ const md = markdownit({
   typographer: true
 });
 
+// Load the main template.
+const pageTemplate = Handlebars.compile(readFileSync('src/template.hbs', 'utf8'), { noEscape: true });
+
+// Load the CSS styles.
+const css = readFileSync(`src/styles.css`, 'utf8');
+const styles = Handlebars.compile('{{{css}}}')({ css });
+
+// Register the handlebars partials.
+Handlebars.registerPartial('styles', '{{styles}}');
+Handlebars.registerPartial('content', '{{content}}');
+
 // Build our HTML pages.
 buildHtmlFromMarkdown('index');
 buildHtmlFromMarkdown('404');
 
-// Copy the CSS.
-copyFileSync('src/styles.css', 'dist/styles.css');
-
 function buildHtmlFromMarkdown(fileName) {
   // Load the page contents.
   const markdownContent = readFileSync(`src/${fileName}.md`, 'utf8');
-  const htmlContent = md.render(markdownContent);
 
-  // Load the html template.
-  Handlebars.registerPartial('content', '{{content}}')
-  const pageTemplate = Handlebars.compile(readFileSync('src/template.hbs', 'utf8'), { noEscape: true });
-  const renderedContent = Handlebars.compile('{{{html}}}')({ html: md.render(htmlContent) });
+  // Render the markdown into a handlebars template.
+  const content = Handlebars.compile('{{{html}}}')({ html: md.render(markdownContent) });
 
   // Generate the html.
-  writeFileSync(`dist/${fileName}.html`, pageTemplate({ content: renderedContent }));
+  writeFileSync(`dist/${fileName}.html`, pageTemplate({ styles, content }));
 }
